3b4a409ac5186a341978cafd685a8b5cb1be48ed
Emil Breustedt <emil.breustedt@gmail.com>
2020-11-17 19:45:35 +0100

    bis 5

--- queries_messenger.sql
+++ queries_messenger.sql
@@ -1,12 +1,12 @@
 USE messengers;
-# Display the name of the customers that belong to the "Farmàcia" category.
+
+# 1) (5 points) Display the name of the customers that belong to the "Farmàcia" category.
 # Sort your results ordered by name (ascending or descending).
 SELECT name FROM customer WHERE category_id in (
 Select id FROM category WHERE name = "Farmàcia");
 
-#Display a list with the categories (name) and its parent category (name),
+#2) (5 points) Display a list with the categories (name) and its parent category (name),
 #where the parent category is "Tecnologia" or "Llibreria".
-Select* From category;
 select parent.name as parent_name, child.name as child_name
 from category child
   inner join category parent
@@ -14,9 +14,49 @@ from category child
     and parent.id in
 (Select id FROM category WHERE name = "Tecnologia" or name = "Llibreria");
 
-#Select the name and distance between customers and offices, where the
+#3) (5 points) Select the name and distance between customers and offices, where the
 #office is identified with "10" and the distance is greater than 1000. Sort results by
 #distance in ascending order.
+SELECT c.name, s.distance from customer c, office o, serve s
+where c.id = s.customer_id and o.id = s.office_id and s.distance > 1000 and o.id = 10
+ORDER BY s.distance asc;
 
-select name, 
+# 4) (5 points) Select the name of the types of vehicles existing in the database and indicate
+# how many vehicles of each type there are. Sort results by number of vehicles in
+# descending order.
+SELECT t.name, (SELECT count(*) as num FROM vehicle v WHERE t.id = v.type_id)
+FROM type t ORDER BY (SELECT count(*) AS num FROM vehicle v WHERE t.id = v.type_id) desc;
 
+# 5) (5 points) Select the customers that can be delivered by the driver identified with the
+# code "10".
+SELECT name FROM customer WHERE id IN 
+(SELECT customer_id FROM serve where office_id = 
+(SELECT office_id FROM driver WHERE id = 10));
+
+# 6) (5 points) Display the identifier and the address of the office closest to a customer with
+# "Restaurant" as a name. Also display what is the distance between them.
+
+SELECT o.id, o.adress, s.distance FROM o.id, o.adress, MIN(s.distance) FROM serve s, office o WHERE o.id = s.office_id AND s.customer_id in
+SELECT MIN(distance) FROM serve
+(SELECT id FROM customer WHERE category_id in
+(SELECT id FROM category WHERE name = "Restaurant"))
+GROUP BY o.id;
+
+/*
+7) (10 points) Display a list of drivers that can drive more than 3 types of vehicles. The list
+must contain the identifier of the driver, the name and the number of types of vehicles
+(that the driver can drive).
+8) (10 points) Display a list with the name, latitude and longitude of the customers that
+have a office with a 2-wheeled vehicle assigned but none of 6 wheels. Also include the
+name of the corresponding vehicle type and remove the duplicates. Sort the result
+ordered by the office name (ascending or descending).
+9) (10 points) Select the model and brand of all vehicles that can be driven by drivers with
+less than 3 years of experience. In addition, vehicles that can be driven by drivers with
+more than 9 years of experience, must be displayed in uppercase (brand and model).
+The result will not contain repeated elements unless they are in both groups.
+10) (10 points) Select the category (name) and the customer (name) categorized by
+"Dentista" or "Farmàcia", which can provide a Mitsubishi or Volkswagen (brand),
+including the number wheels. Display the non-repetitions list and sorted first
+alphabetically and ascending order by the category (name) of the customer. Second, by
+the number of wheels in descending order. Then, sorted by the name of the customer in
+ascending order. And finally, by the type of vehicle also in ascending order.
